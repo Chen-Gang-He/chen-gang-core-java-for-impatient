9866d4a5eb4f4f258e7b29957dac0918c1326976
Chen-Gang <che@merkeinc.com>
2018-08-28 16:52:25 +0800

    Initial commit

---
+++ src/main/java/chengang/com/chapter1/Hello.java
@@ -0,0 +1,10 @@
+package chengang.com.chapter1;
+
+/**
+ * Created by che on 8/28/2018.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
