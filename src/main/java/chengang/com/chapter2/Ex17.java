7bb2f61e796520d53b3f5824ab05039729347184
Chen-Gang <he_chengang@hotmail.com>
2018-10-19 12:06:29 +0800

    ch 2 ex17

--- src/main/java/chengang/com/chapter2/Ex17.java
+++ src/main/java/chengang/com/chapter2/Ex17.java
@@ -1,11 +1,30 @@
 package chengang.com.chapter2;
 
+import java.util.Iterator;
+
 /*
 Implement a class Queue, an unbounded queue of strings. Provide methods add, adding at the tail, and remove, removing at
 the head of the queue. Store elements as a linked list of nodes. Make Node a nested class. Should it be static or not?
  */
 public class Ex17 {
     private Node head;
+
+    private class NodeIterable implements Iterator<Node> {
+        private Node pointer = head;
+
+        @Override
+        public boolean hasNext() {
+            return pointer != null;
+        }
+
+        @Override
+        public Node next() {
+            Node currentPointer = pointer;
+            pointer = pointer.nextNode;
+//            System.out.println("next node: " + pointer.name);
+            return currentPointer;
+        }
+    }
 
     private static class Node {
         Node nextNode;
@@ -20,6 +39,8 @@ public class Ex17 {
             return String.format("Node: %s ", this.name);
         }
     }
+
+
 
 
     private void add(String s) {
@@ -54,14 +75,20 @@ public class Ex17 {
     }
 
     public static void main(String[] args) {
-        Ex17 ex16 = new Ex17();
-        ex16.add("1");
-        ex16.add("2");
-        ex16.add("3");
-        ex16.show();
-        System.out.println("remove head : " + ex16.remove());
-        ex16.add("4");
-        ex16.show();
+        Ex17 ex17 = new Ex17();
+        ex17.add("1");
+        ex17.add("2");
+        ex17.add("3");
+//        ex17.show();
+        System.out.println("remove head : " + ex17.remove());
+        ex17.add("4");
+//        ex17.show();
+        Ex17.NodeIterable iterable = ex17.new NodeIterable();
+//        System.out.println(iterable.pointer);
+        while (iterable.hasNext()) {
+//            Node curentNode = iterable.hasNext();
+            System.out.println(iterable.next());
+        }
 
 
     }
