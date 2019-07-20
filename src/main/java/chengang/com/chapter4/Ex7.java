e6765b74f31de7363f7f882dcaa835d260312808
Chen-Gang <he_chengang@hotmail.com>
2019-07-20 17:16:43 +0800

    chapter 4 7

---
+++ src/main/java/chengang/com/chapter4/Ex7.java
@@ -0,0 +1,11 @@
+package chengang.com.chapter4;
+
+public class Ex7 {
+    enum Color {
+        BLACK, RED, BLUE, GREEN, CYAN, MAGENTA, YELLOW, WHITE;
+        public static Color getRed() {return RED;}
+        public static Color getBlue() {return BLUE;}
+        public static Color getGreen() {return GREEN;}
+
+    }
+}
